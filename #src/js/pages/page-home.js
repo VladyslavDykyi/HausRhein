192b9771ccf622e366e10028e707b8a0565a9003
Vladyslav Dykyi <68863569+VladyslavDykyi@users.noreply.github.com>
2026-05-17 23:58:33 +0300

    дороблені невеличкі фікси, блок buy-now майжез зроблений, блок поточного акціону, підписки ...

--- #src/js/pages/page-home.js
+++ #src/js/pages/page-home.js
@@ -1,7 +1,13 @@
+const delay = document.querySelector('.swiper.mySwiper').dataset.swiperDelay;
+
 const swiper = new Swiper(".mySwiper", {
 	grabCursor: true,
 	slidesPerView: 1,
 	autoHeight: true,
+	autoplay: {
+		delay: Number(delay),
+		disableOnInteraction: false,
+	},
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
@@ -9,5 +15,14 @@ const swiper = new Swiper(".mySwiper", {
 	pagination: {
 		el: ".swiper-pagination",
 		clickable: true,
+	},
+});
+
+var swiper2 = new Swiper(".buy-now-mySwiper", {
+	slidesPerView: 4,
+	spaceBetween: 30,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
 	},
 });
