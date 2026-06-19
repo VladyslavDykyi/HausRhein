04a8f1528ffd0884afa3cf0d5d1b77014b1fe87d
Vladyslav Dykyi <68863569+VladyslavDykyi@users.noreply.github.com>
2026-06-19 20:51:06 +0300

    модальні вікна, та деякі фікси

--- dist/js/pages/page-home.js
+++ dist/js/pages/page-home.js
@@ -17,3 +17,19 @@ const swiper = new Swiper(".mySwiper", {
 		clickable: true,
 	},
 });
+
+
+
+document.addEventListener('click', e => {
+	const btn = e.target.closest('.wrapper-input>.btn-icon');
+	
+	if (!btn) return;
+	
+	const input = btn.closest('.wrapper-input').querySelector('input');
+	
+	input.type = input.type === 'password'
+		? 'text'
+		: 'password';
+	
+	btn.classList.toggle('active');
+});
